8640bec76963ec81b3ed4df12d40c236cb48ea28
Miritoiu Crisiatn George <miritoiu.george@yahoo.com>
2022-11-27 12:28:00 +0200

    Version 2

--- src/Model/PersonalDetails.elm
+++ src/Model/PersonalDetails.elm
@@ -2,6 +2,7 @@ module Model.PersonalDetails exposing (..)
 
 import Html exposing (..)
 import Html.Attributes exposing (class, classList, id)
+import Html.Attributes exposing (href)
 
 
 type alias DetailWithName =
@@ -17,8 +18,27 @@ type alias PersonalDetails =
     , socials : List DetailWithName
     }
 
+{-| Functia transforma un element de tip DetailWithName
+    intr-un list item al unei liste neordonate pentru
+    contacte
+-}
+convertContacts: DetailWithName->Html msg
+convertContacts item=
+    li[][text (item.name++" "++item.detail)]
+
+{-| Functia transforma un element de tip DetailWithName
+    intr-un list item cu un link al unei liste neordonate
+    pentru social links
+-}
+convertLinks: DetailWithName->Html msg
+convertLinks item=
+    li[][a[href item.detail][text item.name]]
 
 view : PersonalDetails -> Html msg
 view details =
-    div [] []
-    -- Debug.todo "Implement the Model.PersonalDetails.view function"
+    div [] 
+        [ h1[id "name"][text details.name]
+        , em[id "intro"][text details.intro]
+        , ul[class "contact-detail"](List.map convertContacts details.contacts)
+        , ul[class "social-link"](List.map convertLinks details.socials)
+        ]
